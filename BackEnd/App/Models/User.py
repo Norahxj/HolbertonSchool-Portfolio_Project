aa0822b30450882806709418d96d0c2fa051029e
manar <mnarzaid247@gmail.com>
2026-05-25 15:32:35 +0300

    creating a user model and test it

---
+++ BackEnd/App/Models/User.py
@@ -0,0 +1,11 @@
+from app.extensions import db
+from app.models.base_model import BaseModel
+
+
+class User(BaseModel):
+    __tablename__ = "users"
+    full_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(20), default="parent")
+    is_active = db.Column(db.Boolean, default=True)
