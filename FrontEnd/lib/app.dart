3fb73cee5b5407c2f77a591cc38de839a837cdc7
root <Nhaljuhani@gmail.com>
2026-06-28 16:26:29 +0200

    FrontEnd Initial Files

---
+++ FrontEnd/lib/app.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'core/constants/app_colors.dart';
+import 'features/onboarding/screens/welcome_screen.dart';
+
+class AsalahApp extends StatefulWidget {
+  const AsalahApp({super.key});
+
+  @override
+  State<AsalahApp> createState() => _AsalahAppState();
+}
+
+class _AsalahAppState extends State<AsalahApp> {
+  Locale _locale = const Locale('ar');
+
+  void _toggleLanguage() {
+    setState(() {
+      _locale = _locale.languageCode == 'ar'
+          ? const Locale('en')
+          : const Locale('ar');
+    });
+  }
+
+  bool get isArabic => _locale.languageCode == 'ar';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Asalah',
+      debugShowCheckedModeBanner: false,
+      locale: _locale,
+      theme: ThemeData(
+        useMaterial3: true,
+        fontFamily: 'Arial',
+        scaffoldBackgroundColor: AppColors.background,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.primary,
+          primary: AppColors.primary,
+        ),
+      ),
+      home: Directionality(
+        textDirection: isArabic ? TextDirection.rtl : TextDirection.ltr,
+        child: WelcomeScreen(
+          isArabic: isArabic,
+          onLanguageToggle: _toggleLanguage,
+        ),
+      ),
+    );
+  }
+}
