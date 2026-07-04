cde0095012c802b05117716205dc3b2970482b56
manar <mnarzaid247@gmail.com>
2026-07-04 22:12:13 +0300

    deleting the back-up backend

--- BackEnd (marshmellow schema)/app/main.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-
-# Create an instance of the Flask application
-app = create_app()
-
-# Run the application if this script is executed directly
-if __name__ == '__main__':
-    app.run(debug=True)
