4be5f18117df00e587b8c3807b3667308ab95e77
Norah Aljuhani <Nhaljuhani@gmail.com>
2026-07-08 21:24:43 +0300

    Fix local environment setup and API endpoints

--- FrontEnd/lib/core/network/api_constants.dart
+++ FrontEnd/lib/core/network/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
-  static String get baseUrl => "http://127.0.0.1:5000/api";
+  static String get baseUrl => "http://127.0.0.1:5000/api/";
 
-  static const String login = "/auth/login";
-  static const String register = "/auth/register";
+  static const String login = "auth/login";
+  static const String register = "auth/register";
 }
