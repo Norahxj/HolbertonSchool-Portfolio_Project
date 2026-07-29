73ea150f487aca9a3763aa5605bb873d8f30c8f2
manar <mnarzaid247@gmail.com>
2026-07-29 16:39:50 +0300

    deploy

--- FrontEnd/lib/features/onboarding/widgets/role_card.dart
+++ FrontEnd/lib/features/onboarding/widgets/role_card.dart
@@ -153,7 +153,7 @@ class _RoleCardState extends State<RoleCard> {
                           ),
                           const SizedBox(width: 8),
                           Transform.translate(
-                            offset: const Offset(0, 4),
+                            offset: const Offset(0, 8),
                             child: Container(
                               width: 44,
                               height: 44,
